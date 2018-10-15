dac22123cd769482e2e9fd1e7493eb1b0a424d8b
Guillermo Figueroa <gfirem@gmail.com>
2018-10-15 10:11:40 -0300

    * Cleaning the code of the submission class

--- classes/class-buddyforms-geo-my-wp-submission.php
+++ classes/class-buddyforms-geo-my-wp-submission.php
@@ -8,56 +8,53 @@
  * @license GPLv2 or later
  */
 
-if (!defined('ABSPATH')) {
-    exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
-class BuddyFormsGeoMyWpSubmission
-{
+class BuddyFormsGeoMyWpSubmission {
 
-    public function __construct()
-    {
-        add_filter('bf_submission_column_default', array($this, 'buddyforms_element_submission_value'), 10, 4);
-    }
+	public function __construct() {
+		add_filter( 'bf_submission_column_default', array( $this, 'buddyforms_element_submission_value' ), 10, 4 );
+	}
 
-    /**
-     * Ge the value to show in the submission list
-     *
-     * @param $bf_value
-     * @param WP_Post $item
-     * @param string $field_type
-     * @param string $field_slug
-     *
-     * @return mixed
-     */
-    public function buddyforms_element_submission_value($bf_value, $item, $field_type, $field_slug)
-    {
-        if ($field_type === 'geo_my_wp_address') {
-            $form_slug = get_post_meta($item->ID, '_bf_form_slug', true);
-            if (empty($form_slug)) {
-                return $bf_value;
-            }
-            $form_type = BuddyFormsGeoMyWpElement::get_buddyforms_form_type($form_slug);
-            if ('registration' !== $form_type) {
-                $field_count = get_post_meta($item->ID, $field_slug . '_count', true);
-            } else {
-                $field_count = get_user_meta($item->post_author, $field_slug . '_count', true);
-            }
-            if (empty($field_count)) {
-                $field_count = 0;
-            }
-            $addresses = array();
-            for ($i = 0; $i <= $field_count; $i++) {
-                if ('registration' !== $form_type) {
-                    $addresses[] = get_post_meta($item->ID, $field_slug . '_' . $i, true);
-                } else {
-                    $addresses[] = get_user_meta($item->post_author, $field_slug . '_' . $i, true);
-                }
-            }
+	/**
+	 * Ge the value to show in the submission list
+	 *
+	 * @param $bf_value
+	 * @param WP_Post $item
+	 * @param string $field_type
+	 * @param string $field_slug
+	 *
+	 * @return mixed
+	 */
+	public function buddyforms_element_submission_value( $bf_value, $item, $field_type, $field_slug ) {
+		if ( $field_type === 'geo_my_wp_address' ) {
+			$form_slug = get_post_meta( $item->ID, '_bf_form_slug', true );
+			if ( empty( $form_slug ) ) {
+				return $bf_value;
+			}
+			$form_type = BuddyFormsGeoMyWpElement::get_buddyforms_form_type( $form_slug );
+			if ( 'registration' !== $form_type ) {
+				$field_count = get_post_meta( $item->ID, $field_slug . '_count', true );
+			} else {
+				$field_count = get_user_meta( $item->post_author, $field_slug . '_count', true );
+			}
+			if ( empty( $field_count ) ) {
+				$field_count = 0;
+			}
+			$addresses = array();
+			for ( $i = 0; $i <= $field_count; $i ++ ) {
+				if ( 'registration' !== $form_type ) {
+					$addresses[] = get_post_meta( $item->ID, $field_slug . '_' . $i, true );
+				} else {
+					$addresses[] = get_user_meta( $item->post_author, $field_slug . '_' . $i, true );
+				}
+			}
 
-            $bf_value = implode('<br/>', $addresses);
-        }
+			$bf_value = implode( '<br/>', $addresses );
+		}
 
-        return $bf_value;
-    }
+		return $bf_value;
+	}
 }
