7ac0104ba4123fb52bbea6fc0cea4d95146b947b
Guillermo Figueroa Mesa (gfirem) <gfirem@gmail.com>
2020-03-09 13:54:46 -0300

    New version

--- classes/class-buddyforms-geo-my-wp-manager.php
+++ classes/class-buddyforms-geo-my-wp-manager.php
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 class BuddyFormsGeoMyWpManager {
-	protected static $version = '1.1.8';
+	protected static $version = '1.1.9';
 	private static $plugin_slug = 'bf_geo_wp';
 
 	public function __construct() {
